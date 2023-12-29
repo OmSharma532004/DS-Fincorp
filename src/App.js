bbe9b2314ce4b313797afa6323955f090bc0ddce
OmSharma532004 <22103203@mail.jiit.ac.in>
2023-12-30 01:06:16 +0530

    added the Homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { Navbar } from './Components/common/Navbar';
+import { Route,Routes } from 'react-router';
+import { Homepage } from './Components/pages/Homepage';
 
 function App() {
   return (
-    <div className=" bg-[#000814] text-white  w-screen min-h-[100vh] overflow-y-auto ">
+    <div className="bg-gradient-to-r from-zinc-700 to-zinc-900 text-white  w-screen min-h-[100vh] overflow-y-auto ">
    <Navbar/>
+   <Routes>
+    <Route path="/" element={<Homepage/>}/>
+   </Routes>
   
     </div>
   );
--- src/Components/common/Navbar.jsx
+++ src/Components/common/Navbar.jsx
@@ -8,22 +8,17 @@ export const Navbar =()=>{
 
     return(
         <>
-        <div className=" border-b-[#2C333F] p-4  relative text-blue-400 w-screen ">
+        <div className=" border-b-[#2C333F] p-4  relative  text-yellow-300 w-screen ">
             <ul className=" flex">
                 <li className=" ml-4 font-bold"><Link to={"/"}>
-                    <img src={logo} width={200} height={50}></img>
-                    {/* <h1 className="text-3xl">DS FINCORP</h1> */}
+                    {/* <img src={logo} width={200} height={50}></img> */}
+                    <h1 className=" text-white text-3xl font-light">DS FINCORP</h1>
                     </Link></li>
 
-                    <li><Link to={"/"}><b className=" absolute left-[40%] text-xl">Home</b></Link></li>
-                    <li><Link to={"/about"}><b className=" absolute left-[50%] text-xl">About</b></Link></li>
-                    <li><Link to={"/contact"}><b className=" absolute left-[60%] text-xl">Contact</b></Link></li>
-                    <li className="absolute text-4xl top-[-5%] left-[38%]">                    <CodeBlocks 
-                position={"lg:flex-row"}
-                codeblock={`You Grow With Us`}
-                codeColor={"blue-500"}
-                backgroudGradient={"grad"}
-            /></li>
+                    <li><Link to={"/"}><p className=" absolute left-[40%] text-xl">Home</p></Link></li>
+                    <li><Link to={"/about"}><p className=" absolute left-[50%] text-xl">About</p></Link></li>
+                    <li><Link to={"/contact"}><p className=" absolute left-[60%] text-xl">Contact</p></Link></li>
+                
 
             </ul>
 
